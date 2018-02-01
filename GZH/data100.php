e2bbc12e605410a80012372cf28271c388cf4f4d
sirm2z <m18367819165@gmail.com>
2018-02-01 16:56:49 +0800

    test GZH

---
+++ GZH/data100.php
@@ -0,0 +1,22 @@
+{
+ "a": {
+  "wxid": "appsolution",
+  "tishi3": "dddddddd",
+  "poiScale": 14,
+  "imgUrl": "http://mmsns.qpic.cn/mmsns/RLllkTm3DUdV24xbZnKicx9jJWxXI0Bq84zzbtibGuRyk/0",
+  "lineLink": "http://wz.bugcom.cn",
+  "poiInfoUrl": "",
+  "imgheight": "640",
+  "showy": 113.23,
+  "poiId": "ianping_2331037",
+  "tishi1": "欢迎进入玩游戏，大家发发发",
+  "wxhao": "MjM5MjAyNDUyMA==",
+  "shareTitle": "快来一起来测试一下你的性格如何!",
+  "postishi": "请在输入框输入:大家朋友来抢红包!",
+  "poiAddress": "bb",
+  "descContent": "快来一起来测试一下你的性格如何",
+  "tishi2": "aaaaaaaa",
+  "imgwidth": "640",
+  "showx": 23.113
+ }
+}
